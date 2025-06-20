617b05d8ee4aaf94e226013de808bd1943903094
spawley1iu <spawley@iu.edu>
2025-06-20 11:55:17 -0400

    updated files

--- src/components/CountyPopup.jsx
+++ src/components/CountyPopup.jsx
@@ -2,21 +2,24 @@ import React from 'react'
 import CountyChart from './CountyChart'
 
 export default function CountyPopup({ feature }) {
-    const props = feature.getProperties()
+    if (!feature) return null
 
-    const aiAn = props.ai_an || 0
-    const poverty = props.poverty || 0
-    const income = props.income || 0
-    const hsGrad = props.hs_grad || 0
+    const props = feature.getProperties ? feature.getProperties() : {}
+
+    const name = props.NAME || 'Unknown County'
+    const aiAn = parseFloat(props.ai_an) || 0
+    const poverty = parseFloat(props.poverty) || 0
+    const income = parseFloat(props.income) || 0
+    const hsGrad = parseFloat(props.hs_grad) || 0
 
     return (
-        <div style={{ fontSize: 14 }}>
-            <h3>{props.NAME}</h3>
+        <div style={{ fontSize: 14, maxWidth: 260 }}>
+            <h3 style={{ marginTop: 0 }}>{name}</h3>
             <ul style={{ paddingLeft: 16 }}>
                 <li>AI/AN Population: {aiAn.toFixed(1)}%</li>
                 <li>Poverty Rate: {poverty.toFixed(1)}%</li>
                 <li>Median Income: ${income.toLocaleString()}</li>
-                <li>High School Graduates: {hsGrad.toFixed(1)}%</li>
+                <li>HS Diploma: {hsGrad.toFixed(1)}%</li>
             </ul>
             <CountyChart data={[aiAn, poverty, income, hsGrad]} />
         </div>
